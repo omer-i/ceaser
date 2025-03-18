a37c0b0c158d21ac2585a4ca06e221f1fc97a974
omer-i <omerisraelo@gmail.com>
2025-03-18 14:42:32 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 import string
-
+from collections import OrderedDict
+sorted_dic = {}
 def decrypt(encrypted_message):
     alphabet = string.ascii_lowercase
-    frequancy = ["e", "t", "a", "o", "i","n" "s", "r","h","d","l","u","c","m","f","y","w","g","p","b","v","k","x","q","j","z"]
+    frequancy = "etaoinsrhdlucmfywgpbvkxqjz"
     decrypted_message = ""
     dicenc = {}
     for c in encrypted_message:
-        if c in dicenc:
+        if c.isalpha():
+          if c in dicenc:
             dicenc[c]+= 1
-        else:
+          else:
             dicenc[c] = 1
-    sorted_dic = {k: v for k, v in sorted(dicenc.items(), key=lambda item: item[1], reverse=True)}
 
+    sorted_dic = dict(sorted(dicenc.items(), key=lambda item: item[1], reverse=True))
     print(sorted_dic)
-    #for char in sorted_dic:
 
 
 
-    return decrypted_message
+    return sorted_dic
 
 encrypted_message = "pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba."
-print(decrypt(encrypted_message))
+sorted_dic = decrypt(encrypted_message)
